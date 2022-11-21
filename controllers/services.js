ba576a4c4ec8d784c024f5df530eb09b71cd715e
Kathy White <kawhite8503@gmail.com>
2022-11-21 17:58:13 -0600

    update database with services

--- controllers/services.js
+++ controllers/services.js
@@ -1,10 +1,36 @@
-import { Service } from "../models/service";
+import { Service } from "../models/service.js";
 
 function create(req,res){
-  
+  req.body.owner = req.user.profile
+  Service.create(req.body)
+  .then(service => {
+    Service.findById(service._id)
+    .populate('owner')
+    .then(populatedService => {
+      res.json(populatedService)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
+}
+
+function index(req,res) {
+  Service.find({})
+  .populate('owner')
+  .then(services => {
+    res.json(services)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
 }
 
 
 export {
+  create,
+  index,
 
 }
--- routes/services.js
+++ routes/services.js
@@ -5,10 +5,10 @@ import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 const router = Router()
 
 /*---------- Public Routes ----------*/
-
+router.get('/', servicesCtrl.index)
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
-
+router.post('/', checkAuth, servicesCtrl.create)
 
 export { router }
